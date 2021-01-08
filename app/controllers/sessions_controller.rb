2400bc0be0eb373b02ce0a0c1a6f307333c23444
Jacqueline Freeland <secretartist272@gmail.com>
2021-01-08 08:40:57 -0500

    added signup, login, home

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,17 @@
 class SessionsController < ApplicationController
+    #user signup
+    def signup
+
+    end
+
+    #user login
+    def login
+
+    end
+
+     #homepage
+    def home
+        render "homepage"
+    end
 
 end

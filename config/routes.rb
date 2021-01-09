a38c0f557e743d0f854d34d8fd314dac7c37d9e4
Jacqueline Freeland <secretartist272@gmail.com>
2021-01-09 14:45:30 -0500

    refactored link_to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   resources :research_subjects
   resources :researches
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   #homepage
   get 'home', to: 'sessions#home'
   get '/signup', to: 'sessions#signup'
   get 'login', to: 'sessions#login'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  
 end

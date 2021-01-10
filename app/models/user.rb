256a1aba23dba23f0fad37cc34a5e863b8640ea6
Jacqueline Freeland <secretartist272@gmail.com>
2021-01-09 23:56:31 -0500

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
     
+    validates :name, presence: true
+    validates :email, presence: true 
+    validates :age, presence: true
+    validates :password_digest, presence: true, length: {in: 6..15}
 end

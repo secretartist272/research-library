1a2592529d945f46be9afd966d6e0f1080960db5
Jacqueline Freeland <secretartist272@gmail.com>
2021-01-11 15:54:47 -0500

    added validations and belongs_to/has_many

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
     has_secure_password
     
+    #validations
     validates :name, presence: true
-    validates :email, presence: true 
+    validates :email, presence: true, format: /@/ 
     validates :age, presence: true
-    validates :password_digest, presence: true, length: {in: 6..15}
+    validates :password, presence: true, uniqueness: { case_sensitive: true }, length: {in: => 6..20}
+
+    #has_many and belongs_to
+    has_many :research, through: :research_subjects
+    has_many :research_subjects
 end

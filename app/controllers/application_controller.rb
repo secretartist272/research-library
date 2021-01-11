68e4ee78755d19c42463ce1c47d47c217abd231d
Jacqueline Freeland <secretartist272@gmail.com>
2021-01-11 18:48:02 -0500

    added helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
+    helper_method current_user, logged_in?
 
-   
+   private
+
+   def current_user
+        @user = user.find_by_id(session[:user_id])
+   end
+
+   def logged_in?
+        !!current_user
+   end
+
     
 end

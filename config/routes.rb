df6b29f5b1f275d5bae66347c1a076a8c25ae835
Jacqueline Freeland <secretartist272@gmail.com>
2021-01-11 17:56:59 -0500

    refactored routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   resources :research_subjects
   resources :researches
   resources :users
-  #homepage routes
-  get '/', to: 'sessions#home'
+  
+  #root path
+  root 'sessions#home'
   #signups routes
   get '/signup', to: 'sessions#signup'
   post '/create', to: 'sessions#create'
@@ -13,7 +14,7 @@ Rails.application.routes.draw do
   post '/success', to: 'sessions#new'
 
   #Logout route
-  get '/logout', to: 'sessions#logout'
+  delete '/logout', to: 'sessions#logout'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 

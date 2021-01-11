c253c7fdbb74e7f2962c8ed7399d18dc7a231977
Jacqueline Freeland <secretartist272@gmail.com>
2021-01-11 16:56:20 -0500

    added logout route

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#login'
   post '/sucess', to: 'sessions#new'
 
+  #Logout route
+  get '/logout', to: 'sessions#logout'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   

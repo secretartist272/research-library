bb9c7fd2d374a4b896e66522d1a0d98c090e80d4
Jacqueline Freeland <secretartist272@gmail.com>
2021-01-11 16:50:39 -0500

    added post route

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,16 @@ Rails.application.routes.draw do
   resources :research_subjects
   resources :researches
   resources :users
-  #homepage
+  #homepage routes
   get '/', to: 'sessions#home'
+  #signups routes
   get '/signup', to: 'sessions#signup'
   post '/create', to: 'sessions#create'
-  get 'login', to: 'sessions#login'
+  
+  #login routes
+  get '/login', to: 'sessions#login'
+  post '/sucess', to: 'sessions#new'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   

b6c84b93e2baf1e961bd8c7da245e6c6af2d2886
Jacqueline Freeland <secretartist272@gmail.com>
2021-01-11 19:42:03 -0500

    corrected code error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
    private
 
    def current_user
-        @user = user.find_by_id(session[:user_id])
+        @user = User.find_by_id(session[:user_id])
    end
 
    def logged_in?

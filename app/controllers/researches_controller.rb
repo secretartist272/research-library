053ca57479ec1719ed6a0e6a5c870bb71e9176f4
Jacqueline Freeland <secretartist272@gmail.com>
2021-01-13 00:02:44 -0500

    added helper and refactored new

--- app/controllers/researches_controller.rb
+++ app/controllers/researches_controller.rb
@@ -1,18 +1,34 @@
 class ResearchesController < ApplicationController
 
     def index
-        @research = Research.all 
+        @researches = Researches.all 
     end
 
     def show
-        @research = Research.all
+        
     end
 
+    
     def new
-        @research = Research.new
+        @research = Researches.new(research_params)
+        
+        if @research.save 
+            flash[:notice] = "Your research has been logged successfully!"
+            redirect_to research_path
+        else        
+            flash[:notice] = "Let's try that again"
+            render :new
+        end
+        
     end
 
     def create
 
+    end
+
+    private
+
+    def research_params
+        params.require(:researches).permit(:title, :description)
     end
 end

0ef657926dcf02e99be4b4ee1b73831256993977
Jacqueline Freeland <secretartist272@gmail.com>
2021-01-13 15:44:25 -0500

    added validations andd belongs_to

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,7 @@
 class Subject < ApplicationRecord
+    validations :Genre, uniqueness: true, presence: true
+    validations :subject, presence: true
+    belongs_to :user
+    belongs_to :research
+
 end

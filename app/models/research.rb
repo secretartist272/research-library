e65bc8ac5e3a65cecd5d36d70e619f610ac39a69
Jacqueline Freeland <secretartist272@gmail.com>
2021-01-13 15:44:01 -0500

    added validations and has_many

--- app/models/research.rb
+++ app/models/research.rb
@@ -1,2 +1,8 @@
 class Research < ApplicationRecord
+    has_many :users, through: :subject
+    has_many :subjects, through: :user
+
+    validations :title, presence: true, uniquesness: true
+    validations :description, presence: true, length: {255..30,000}
+
 end

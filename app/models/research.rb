997527b7663a20ec3912f17c6d9f01336a811d89
Jacqueline Freeland <secretartist272@gmail.com>
2021-01-14 01:18:26 -0500

    corrected mispelling

--- app/models/research.rb
+++ app/models/research.rb
@@ -2,7 +2,7 @@ class Research < ApplicationRecord
     has_many :users, through: :subject
     has_many :subjects, through: :user
 
-    validations :title, presence: true, uniquesness: true
-    validations :description, presence: true, length: {255..30,000}
+    validators :title, presence: true, uniquesness: true
+    validators :description, presence: true, length: {255..30,000}
 
 end

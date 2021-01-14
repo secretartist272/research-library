4941c8e2718d2988b7b1647237489ebea5893931
Jacqueline Freeland <secretartist272@gmail.com>
2021-01-14 01:18:50 -0500

    corrected mispelling

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,6 +1,6 @@
 class Subject < ApplicationRecord
-    validations :Genre, uniqueness: true, presence: true
-    validations :subject, presence: true
+    validates :genre, uniqueness: true, presence: true
+    validates :subject, presence: true
     belongs_to :user
     belongs_to :research
 

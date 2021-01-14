ac188e4cbfce210531854febacc817c5d4e6c671
Jacqueline Freeland <secretartist272@gmail.com>
2021-01-14 01:18:06 -0500

    refactored new

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -6,4 +6,26 @@ class SubjectsController < ApplicationController
     def show
 
     end
+
+    def new
+        @subject = Subject.new
+    end
+
+    def create 
+        @subject = Subject.new(subject_params)
+        @user = current_user
+        @research = Research.find_by(id: params[:research][:id])
+        
+        if @subject.save
+            redirect_to subject_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def subject_params
+        params.require(:subject).permit(:genre, :subject, :user_id, :research_id)
+    end
 end

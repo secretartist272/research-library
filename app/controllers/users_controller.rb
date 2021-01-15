e3d8655ae09e40a24535b387290670eb1ea44948
Jacqueline Freeland <secretartist272@gmail.com>
2021-01-14 23:25:56 -0500

    created edit and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,22 @@ class UsersController < ApplicationController
     def show
 
     end
+
+    def edit
+       @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+       # binding.pry
+        if @user.update(params[:user][:id])
+            # Handle a successful update.
+             redirect_to user_path(@user) 
+              
+        else
+              render 'edit'
+        end
+    end
+
+
 end

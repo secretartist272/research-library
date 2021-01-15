1dc16512c927312011df5c8397f38b81ddf736aa
Jacqueline Freeland <secretartist272@gmail.com>
2021-01-15 11:52:08 -0500

    refactored code

--- app/controllers/researches_controller.rb
+++ app/controllers/researches_controller.rb
@@ -5,30 +5,31 @@ class ResearchesController < ApplicationController
     end
 
     def show
-        
+        @research = Research.find(params[:id])
     end
 
     
     def new
         @research = Research.new
         
-        if @research.save 
-            flash[:notice] = "Your research has been logged successfully!"
-            redirect_to research_path
-        else        
-            flash[:notice] = "Let's try that again"
-            render :new
-        end
+        
         
     end
 
     def create
-
+        @research = Research.new(research_params)
+        if @research.save 
+            flash[:notice] = "Your research has been logged successfully!"
+            redirect_to research_path(@research)
+        else        
+            flash[:notice] = "Let's try that again"
+            render :new
+        end
     end
 
     private
 
     def research_params
-        params.require(:researches).permit(:title, :description)
+        params.require(:research).permit(:title, :description)
     end
 end

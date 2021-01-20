947c3776865af7f529c2ac5a8e4f396bbb14961d
Jacqueline Freeland <secretartist272@gmail.com>
2021-01-20 18:25:02 -0500

    corrected validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
     #validations
     validates :name, presence: true
     validates :email, presence: true, format: /@/ 
-    validates :age, presence: true
+    #validates :age, presence: true
     #validates :password, presence: true, uniqueness: { case_sensitive: true }, length: {in: 6..20}
 
     #has_many and belongs_to
